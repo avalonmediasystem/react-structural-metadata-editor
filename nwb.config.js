8b2bcde2ea1f346753cd61b7f1387659b55a976f
Adam Arling <adam.arling@northwestern.edu>
2019-04-09 12:09:06 -0500

    Test new build with devDependencies moved to dependencies

--- nwb.config.js
+++ nwb.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   type: 'react-component',
   npm: {
-    esModules: true
-    // umd: {
-    //   global: 'ReactStructuralMetadataEditor',
-    //   externals: {
-    //     react: 'React'
-    //   }
-    // }
+    esModules: true,
+    umd: {
+      global: 'ReactStructuralMetadataEditor',
+      externals: {
+        react: 'React'
+      }
+    }
   }
 };

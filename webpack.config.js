509eadf93ddcc4774c0630799d6d5c62c1aedec8
Dananji <dan9131@gmail.com>
2021-12-15 15:05:28 -0500

    Add gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, 'demo/src/index.html'),
-  filename: './index.html'
+  filename: './index.html',
 });
 module.exports = {
   entry: path.join(__dirname, 'demo/src/index.js'),
+  output: {
+    path: path.join(__dirname, 'demo/dist'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [htmlWebpackPlugin],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
-    port: 3001
+    port: 3001,
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
 };

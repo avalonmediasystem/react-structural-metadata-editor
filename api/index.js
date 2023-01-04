0cbb4e034cee258097974c33bf20e8a6bb2f8ff7
dananji <dwithana@iu.edu>
2023-01-04 09:53:58 -0500

    API deploy

--- api/index.js
+++ api/index.js
@@ -70,33 +70,6 @@ app.get('/media.mp4', (req, res) => {
   fs.createReadStream(path.join(__dirname, 'assets', 'media.mp4')).pipe(res);
 });
 
-app.get('/lunchroom_manners/low/media.mp4', (req, res) => {
-  res.header('Content-Type', 'video/mp4');
-  res.sendFile(path.join(__dirname, 'assets/lunchroom_manners/low', 'lunchroom_manners_256kb.mp4'));
-});
-app.get('/lunchroom_manners/medium/media.mp4', (req, res) => {
-  res.header('Content-Type', 'video/mp4');
-  res.sendFile(path.join(__dirname, 'assets/lunchroom_manners/medium', 'lunchroom_manners_512kb.mp4'));
-});
-app.get('/lunchroom_manners/high/media.mp4', (req, res) => {
-  res.header('Content-Type', 'video/mp4');
-  res.sendFile(path.join(__dirname, 'assets/lunchroom_manners/high', 'lunchroom_manners_1024kb.mp4'));
-});
-
-app.get('/manifest.json', (req, res) => {
-  res.header('Content-Type', 'application/json');
-  let manifest;
-  try {
-    manifest = fs.readFileSync(
-      path.join(__dirname, 'assets/lunchroom_manners', 'manifest.json'),
-      'utf-8'
-    );
-  } catch (err) {
-    console.error('Server -> Error fetching manifest -> ', err);
-  }
-  res.send(manifest);
-});
-
 app.post('/structure.json', (req, res) => {
   const newStructure = req.body.json;
   const cleanedStruct = cleanStructure(newStructure);
